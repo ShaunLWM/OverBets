f1556a84a198cf303448305983eca5acf6e1a330
ShaunLWM <shaunlimweimin@gmail.com>
2020-01-27 11:40:54 +0800

    Add simple cookie authentication

--- server/index.js
+++ server/index.js
@@ -2,14 +2,34 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const session = require("express-session");
+const cors = require("cors");
+const cookieParser = require("cookie-parser"); // parse cookie header
 
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const passportSetup = require("./lib/Passport");
 const database = require("./lib/Database");
 
-const port = 3001;
+app.use(cors({
+    origin: "http://localhost:3001", // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
+}));
 
+app.use(cookieSession({
+    name: "session",
+    keys: [process.env.SERVER_COOKIE_KEY],
+    maxAge: 24 * 60 * 60 * 100,
+}));
+
+app.use(cookieParser());
+app.use(passport.initialize());
+// deserialize cookie from the browser
+app.use(passport.session());
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,4 +55,4 @@ io.on("connection", (socket) => {
     console.log(`[+] socket connected: ${socket.id}`);
 });
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+server.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}!`));
