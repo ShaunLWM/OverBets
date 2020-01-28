67e0e5f3b7724574176e0e145552c408140498c7
ShaunLWM <shaunlimweimin@gmail.com>
2020-01-28 22:08:35 +0800

    Add random steam profile image

--- client/src/components/MatchBets/index.js
+++ client/src/components/MatchBets/index.js
@@ -1,7 +1,7 @@
 import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect, useState } from "react";
-import { getRandomColor, getRandomInt } from "../../lib/Helper";
+import { getRandomAvatar, getRandomColor, getRandomInt } from "../../lib/Helper";
 import BetsContainer from "../BetsContainer";
 
 const useStyles = makeStyles(({ spacing, palette }) => {
@@ -25,7 +25,7 @@ const KanbanCard = ({ className }) => {
     useEffect(() => {
         setDemoPlayers(new Array(getRandomInt(1, 8)).fill("").map(() => {
             return {
-                img: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/80/80683e910ec2b00eb8903009b08a755e65d94349_full.jpg",
+                img: getRandomAvatar(),
                 coins: 100,
                 color: getRandomColor()
             }
