35c37f8434386c8c8b3408050d51cdc0aafbfffa
ShaunLWM <shaunlimweimin@gmail.com>
2020-02-01 13:24:15 +0800

    Allow token in GET method

--- server/lib/isAuthenticated.js
+++ server/lib/isAuthenticated.js
@@ -6,7 +6,15 @@ function isAuthenticated(req, res, next) {
     const token = (bearerToken.replace("Bearer ", "")).trim();
     return jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
         if (error) return res.status(403).send({ success: false, msg: "corrupted token" });
-        if (req.body) req.body.uid = decoded.user_id;
+        switch (req.method) {
+            case "POST":
+                req.body.uid = decoded.user_id;
+                break;
+            case "GET":
+                req.user = { token };
+                break;
+        }
+
         return next();
     });
 }
